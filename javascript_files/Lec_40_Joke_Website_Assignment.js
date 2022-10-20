fc39ad7874e637d7e4251a2781ddb29c87ddd4f2
Ahmed Ummar <ummarahmed001@gmail.com>
2022-10-20 03:48:19 +0300

    Joke Teller Website Created
    I made a website which will take a number from you and will show you a number. Moreover, if you enter any wrong input(value other than a number) then it will restrict you to do so and warn you.

---
+++ javascript_files/Lec_40_Joke_Website_Assignment.js
@@ -0,0 +1,449 @@
+let all_jokes = ["1. What do you call a boomerang that won’t come back?\nA stick."
+    ,
+    "2. What does a cloud wear under his raincoat?\nThunderwear."
+    ,
+    "3. Two pickles fell out of a jar onto the floor. What did one say to the other?\nDill with it."
+    ,
+    "4. What time is it when the clock strikes 13?\nTime to get a new clock."
+    ,
+    "5. How does a cucumber become a pickle?\nIt goes through a jarring experience.\nJokes for kids"
+    ,
+    "6. What did one toilet say to the other?\nYou look a bit flushed."
+    ,
+    "7. What do you think of that new diner on the moon?\nFood was good, but there really wasn’t much atmosphere."
+    ,
+    "8. Why did the dinosaur cross the road?\nBecause the chicken wasn’t born yet."
+    ,
+    "9. Why can’t Elsa from Frozen have a balloon?\nBecause she will let it go, let it go."
+    ,
+    "10. What musical instrument is found in the bathroom?\nA tuba toothpaste."
+    ,
+    "Related: Funny Christian Jokes"
+    ,
+    "11. Why did the kid bring a ladder to school?\nBecause she wanted to go to high school."
+    ,
+    "12. What do you call a dog magician?\nA labracadabrador."
+    ,
+    "13. Where would you find an elephant?\nThe same place you lost her.\nJokes for kids"
+    ,
+    "14. How do you get a squirrel to like you?\nAct like a nut."
+    ,
+    "15. What do you call two birds in love?\nTweethearts"
+    ,
+    "16. How does a scientist freshen her breath?\nWith experi-mints."
+    ,
+    "17. How are false teeth like stars?\nThey come out at night."
+    ,
+    "18. What building in your town has the most stories?\nThe public library."
+    ,
+    "19. What’s worse than finding a worm in your apple?\nFinding half a worm."
+    ,
+    "20. What is a computers favorite snack?\nComputer chips."
+    ,
+    "21. What did one volcano say to the other?\nI lava you."
+    ,
+    "22. How do we know that the ocean is friendly?\nIt waves."
+    ,
+    "23. What is a tornado’s favorite game to play?\nTwister."
+    ,
+    "24. How does the moon cut his hair?\nEclipse it."
+    ,
+    "25. How do you talk to a giant?\nUse big words."
+    ,
+    "Related: 101 Knock Knock Jokes for Kids"
+    ,
+    "26. What animal is always at a baseball game?\nA bat."
+    ,
+    "27. What falls in winter but never gets hurt?\nSnow."
+    ,
+    "28. What did the Dalmatian say after lunch?\nThat hit the spot."
+    ,
+    "29. Why did the kid cross the playground?\nTo get to the other slide."
+    ,
+    "30. What do you call a droid that takes the long way around?\nR2 detour."
+    ,
+    "31. Why did the cookie go to the hospital?\nBecause he felt crummy."
+    ,
+    "32. Why was the baby strawberry crying?\nBecause her mom and dad were in a jam."
+    ,
+    "33. What did the little corn say to the mama corn?\nWhere is pop corn?"
+    ,
+    "34. How do you make a lemon drop?\nJust let it fall."
+    ,
+    "35. What did the limestone say to the geologist?\nDon’t take me for granite."
+    ,
+    "36. Why does a seagull fly over the sea?\nBecause if it flew over the bay, it would be a baygull."
+    ,
+    "37. What kind of water can’t freeze?\nHot water."
+    ,
+    "38. What kind of tree fits in your hand?\nA palm tree."
+    ,
+    "39. What do you call a dinosaur that is sleeping?\nA dino-snore."
+    ,
+    "40. What is fast, loud and crunchy?\nA rocket chip."
+    ,
+    "41. Why did the teddy bear say no to dessert?\nBecause she was stuffed."
+    ,
+    "42. What has ears but cannot hear?\nA cornfield."
+    ,
+    "43. What did the left eye say to the right eye?\nBetween us, something smells."
+    ,
+    "44. What did one plate say to the other plate?\nDinner is on me."
+    ,
+    "45. Why did the student eat his homework?\nBecause the teacher told him it was a piece of cake."
+    ,
+    "46. When you look for something, why is it always in the last place you look?\nBecause when you find it, you stop looking."
+    ,
+    "47. What is brown, hairy and wears sunglasses?\nA coconut on vacation."
+    ,
+    "48. What do you say to a rabbit on its birthday?\nHoppy Birthday."
+    ,
+    "49. What’s the one thing will you get every year on your birthday, guaranteed?\nA year older."
+    ,
+    "50. Why do candles always go on the top of cakes?\nBecause its hard to light them from the bottom."
+    ,
+    "51. What do cakes and baseball teams have in common?\nThey both need a good batter."
+    ,
+    "52. What goes up but never comes down?\nYour age."
+    ,
+    "53. What does every birthday end with?\nThe letter Y."
+    ,
+    "54. What did the tiger say to her cub on his birthday?\nIts roar birthday."
+    ,
+    "55. Why did the girl put her cake in the freezer?\nShe wanted to ice it."
+    ,
+    "56. Does a green candle burn longer than a pink one?\nNo, they both burn shorter."
+    ,
+    "57. Why did the little girl hit her birthday cake with a hammer?\nIt was a pound cake.\nFunny Yo Mama Jokes for Kids"
+    ,
+    "58. Yo Mama so small her best friend is an ant."
+    ,
+    "59. Yo Mama so old God signed her yearbook."
+    ,
+    "60. Yo Mama so short she has to hold a sign up that says, Dont spit, I cant swim."
+    ,
+    "61. Yo Mama so small she has to slam-dunk her bus fare."
+    ,
+    "62. Yo Mama so old she rode dinosaurs to school."
+    ,
+    "63. Yo Mama so old her memory is in black and white.\nFunny Kids Jokes about Math Jokes for kids"
+    ,
+    "64. Why didnt the quarter roll down the hill with the nickel?\nBecause it had more cents."
+    ,
+    "65. Why is the obtuse triangle always so frustrated?\nBecause it’s never right."
+    ,
+    "66. Why is six afraid of seven?\nBecause seven eight nine."
+    ,
+    "67. Why was the equal sign so humble?\nBecause he wasn’t greater than or less than anyone else."
+    ,
+    "68. What do you call guys who love math?\nAlgebros."
+    ,
+    "69. How do you stay warm in any room?\nGo to the corner—it’s always 90 degrees."
+    ,
+    "70. Why was the fraction nervous about marrying the decimal?\nBecause he would have to convert."
+    ,
+    "71. Are monsters good at math?\nNot unless you count Dracula."
+    ,
+    "72. Why was the math book sad?\nBecause it had too many problems."
+    ,
+    "73. Why does nobody talk to circles?\nBecause there’s no point."
+    ,
+    "74. Why couldnt the pony sing a lullaby?\nShe was a little horse."
+    ,
+    "75. What was the first animal in space?\nThe cow that jumped over the moon."
+    ,
+    "76. Why don’t elephants chew gum?\nThey do, just not in public."
+    ,
+    "77. What did the banana say to the dog?\nBananas can’t talk."
+    ,
+    "78. How do you make an octopus laugh?\nWith ten-tickles."
+    ,
+    "79. What do you call a sleeping bull?\nA bull-dozer."
+    ,
+    "80. How do you fit more pigs on a farm?\nBuild a sty-scraper."
+    ,
+    "81. What did the farmer call the cow that had no milk?\nAn udder failure."
+    ,
+    "82. What do you call a cow that wont give milk?\nA milk dud."
+    ,
+    "83. Why do fish live in salt water?\nBecause pepper makes them sneeze."
+    ,
+    "84. What do you get from a pampered cow?\nSpoiled milk."
+    ,
+    "85. Where do polar bears vote?\nThe North Poll"
+    ,
+    "86. What sound do porcupines make when they kiss?\nOuch!"
+    ,
+    "87. Why did the snake cross the road?\nTo get to the other ssside."
+    ,
+    "88. Why are fish so smart?\nBecause they live in schools."
+    ,
+
+    "89. What did the ocean say to the pirate?\nNothing, it just waved."
+    ,
+    "90. Why don’t pirates shower before they walk the plank?\nBecause they’ll just wash up on shore later."
+    ,
+    "91. What happened when Bluebeard fell overboard in the Red Sea?\nHe got marooned."
+    ,
+    "92. How did the pirate get his flag so cheaply?\nHe bought it on sail."
+    ,
+    "93. What has 8 legs, 8 arms, and 8 eyes?\n8 pirates."
+    ,
+    "94. How much does it cost a pirate to get his ears pierced?\nAbout a buck an ear."
+    ,
+    "95. Why is pirating so addictive?\nThey say once ye lose yer first hand, ye get hooked."
+    ,
+    "96. How do pirates know that they are pirates?\nThey think, therefore they arrr."
+    ,
+    "See more funny Pirate Jokes.\nFunny Kids Jokes About Cats"
+    ,
+    "97. What is a cats favorite color?\nPurrr-ple."
+    ,
+    "98. What song does a cat like best?\nThree Blind Mice."
+    ,
+    "99. Where did the school kittens go for their field trip?\nTo the mew-seum."
+    ,
+    "100. What kind of kitten works for the Red Cross?\nA first-aid kit."
+    ,
+    "Related: Brain Teasers for Kids"
+    ,
+    "101. Why are cats good at video games?\nBecause they have nine lives."
+    ,
+    "102. What did the cat say when he fell off the table?\nMe-ow."
+    ,
+    "103. What is the difference between a cat that got photocopied and a cat that follows you?\nOne is a cat copy; the other is a copy cat."
+    ,
+    "104. What do you get when you cross a ball and a cat?\nA fur ball."
+    ,
+    "105. Whats a cats favorite magazine?\nA cat-alogue."
+    ,
+    "106. What cat likes living in water?\nAn octo-puss."
+    ,
+    "See more Funny Cat Jokes.\nFunny Halloween Kid Jokes"
+    ,
+    "107. Why didn’t the skeleton go to school?\nHis heart wasn’t in it."
+    ,
+    "108. How does a vampire start a letter?\nTomb it may concern..."
+    ,
+    "109. What is a monster’s favorite dessert?\nI scream."
+    ,
+    "110. What monster plays tricks on Halloween?\nPrank-enstein."
+    ,
+    "111. What kind of music do mummies love?\nWrap music."
+    ,
+    "112. What fruit do scarecrows love the most?\nStraw-berries."
+    ,
+    "113. What does a witch use to do her hair?\nScarespray."
+    ,
+    "114. What room does a ghost not need?\nA living room."
+    ,
+    "115. What kind of dog does Dracula have?\nA blood hound."
+    ,
+    "116. What is a ghost’s nose full of?\nBoo-gers."
+    ,
+    "117. What do birds say on Halloween?\nTrick or tweet."
+    ,
+    "118. Are black cats bad luck?\nSure, if you’re a mouse."
+    ,
+    "119. How do you fix a cracked pumpkin?\nA pumpkin patch."
+    ,
+    "120. When is it bad luck to be followed by a black cat?\nWhen you’re a mouse."
+    ,
+    "121. What do you call two witches living together?\nBroommates."
+    ,
+    "122. What happens when a vampire goes in the snow?\nFrost bite."
+    ,
+    "123. Why did the zombie skip school?\nHe was feeling rotten."
+    ,
+    "124. What is a vampire’s favorite fruit?\nA blood orange."
+    ,
+    "125. What instrument does a skeleton play?\nThe trom-bone."
+    ,
+    "126. Where do baby ghosts go during the day?\nDay-scare centers."
+    ,
+    "127. Why didn’t the skeleton go to the dance?\nBecause he had no body to go with."
+    ,
+    "128. What candy do you eat on the playground?\nRecess pieces."
+    ,
+    "129. How do ghosts wash their hair?\nWith sham-boo."
+    ,
+    "130. What’s a witch’s favorite subject in school?\nSpelling."
+    ,
+    "131. What’s big, scary and has three wheels?\nA monster on a tricycle."
+    ,
+    "132. Why don’t vampires have more friends?\nBecause they are a pain in the neck."
+    ,
+    "133. What position does a ghost play in hockey?\nGhoulie"
+    ,
+    "134. What do you call a witch who goes to the beach?\nA sand-witch."
+    ,
+    "135. What do you give a vampire when he’s sick?\nCoffin drops."
+    ,
+    "136. What kinds of pants do ghosts wear?\nBoo-jeans."
+    ,
+    "See more Funny Halloween Jokes.\nFunny Thanksgiving Kid Jokes"
+    ,
+    "137. Who isnt hungry at Thanksgiving?\nThe turkey—he’s already stuffed."
+    ,
+    "138. Can a turkey jump higher than Mount Everest?\nYes, because a building can’t jump at all."
+    ,
+    "139. Which side of the turkey has the most feathers?\nThe outside."
+    ,
+    "140. What always comes at the end of Thanksgiving?\nThe letter g."
+    ,
+    "141. What’s the key to a great Thanksgiving dinner?\nThe tur-key."
+    ,
+    "142. Where does Christmas come before Thanksgiving?\nIn the dictionary."
+    ,
+    "143. Why did pilgrims’ pants always fall down?\nBecause they wore their belt buckle on their hat."
+    ,
+    "144. What do turkeys and teddy bears have in common?\nThey both have stuffing."
+    ,
+    "145. What key wont open any door?\nA turkey."
+    ,
+    "146. Why did the turkey cross the road?\nIt was the chickens day off."
+    ,
+    "147. Why did the chewing gum cross the road?\nIt was stuck on the turkeys foot."
+    ,
+    "148. Why did the turkey cross the road twice?\nTo show he wasnt a chicken."
+    ,
+    "149. What do you get when a turkey lays an egg on top of a barn?\nAn eggroll."
+    ,
+    "150. Why was the turkey the drummer in the band?\nBecause he had drumsticks."
+    ,
+    "151. What’s the best thing to put into pumpkin pie?\nYour teeth."
+    ,
+    "152. What’s the best dance to do on Thanksgiving?\nThe turkey trot."
+    ,
+    "153. Why did the Pilgrims sail from England to America?\nBecause they missed their plane."
+    ,
+    "154. When the Pilgrims landed, where did they stand?\nOn their feet."
+    ,
+    "155. Why did the police arrest the turkey?\nThey suspected it of fowl play."
+    ,
+    "156. What should you wear to Thanksgiving dinner?\nA har-vest."
+    ,
+    "157. If the Pilgrims were alive today, what would they be most famous for?\nTheir age."
+    ,
+    "158. Where do you find a turkey with no legs?\nWhere you left it."
+    ,
+    "159. What do you call it when it rains turkeys?\nFoul weather."
+    ,
+    "160. Why did the turkey sit on the tomahawk?\nTo hatch-et."
+    ,
+    "161. What kind of music did Pilgrims listen to?\nPlymouth Rock."
+    ,
+    "162. What smells the best at a Thanksgiving dinner?\nYour nose."
+    ,
+    "163. Why do turkeys always say, “gobble, gobble”?\nBecause they never learned good table manners."
+    ,
+    "See more Funny Thanksgiving Jokes.\nFunny Christmas Jokes for Kids"
+    ,
+    "164. What do elves learn in school?\nThe elf-abet."
+    ,
+    "165. How does a snowman lose weight?\nHe waits for the weather to get warmer."
+    ,
+    "166. What did one snowman say to the other snowman?\nDo you smell carrots?"
+    ,
+    "167. What do you call a reindeer with bad manners?\nRude-olph."
+    ,
+    "168. Why does Santa work at the North Pole?\nBecause the penguins kicked him out of the South Pole."
+    ,
+    "169. What falls at the North Pole and never gets hurt?\nSnow."
+    ,
+    "170. How does a sheep say Merry Christmas?\nFleece Navidad."
+    ,
+    "171. Why did the Christmas tree go to the barber?\nIt needed to be trimmed."
+    ,
+    "172. What is an elf’s favorite kind of music?\nWrap music."
+    ,
+    "173. What kind of photos do elves take?\nElfies."
+    ,
+    "174. What do road crews use at the North Pole?\nSnow cones."
+    ,
+    "175. Why did Rudolph get a bad grade on his report card?\nBecause he went down in history."
+    ,
+    "176. What wears a red suit and goes, “Oh, oh, oh”?\nSanta walking backwards."
+    ,
+    "177. Where does a snowman keep his money?\nIn a snow bank."
+    ,
+    "178. What do you get when you combine a Christmas tree with a computer?\nA pine-apple."
+    ,
+    "179. In what year does New Year’s Day come before Christmas?\nEvery year."
+    ,
+    "180. What do snowmen eat for breakfast?\nFrosted Flakes."
+    ,
+    "181. What kind of motorcycle does Santa like to ride?\nA Holly Davidson."
+    ,
+    "182. Why are Christmas trees bad at sewing?\nBecause they always drop their needles."
+    ,
+    "183. What do you get when Santa becomes a detective?\nSanta clues."
+    ,
+    "184. Why was the Easter Bunny so upset?\nHe was having a bad hare day."
+    ,
+    "185. How did the soggy Easter Bunny dry himself?\nWith a hare dryer."
+    ,
+    "186. How does the Easter bunny stay in shape?\nLots of eggs-ercise."
+    ,
+    "187. Why can’t a rabbit’s nose be 12 inches long?\nBecause then it would be a foot."
+    ,
+    "188. How can you tell which rabbits are the oldest in a group?\nJust look for the gray hares."
+    ,
+    "189. What do you call a bunny who isn’t smart?\nA hare brain."
+    ,
+    "190. What’s the best way to catch a unique rabbit?\nUnique up on him."
+    ,
+    "191. What happened when the Easter Bunny met the rabbit of his dreams?\nThey lived hoppily ever after."
+    ,
+    "192. What do you call a line of rabbits walking backwards?\nA receding hareline."
+    ,
+    "193. Why shouldn’t you tell an Easter egg a good joke?\nIt might crack up."
+    ,
+    "194. What did one colored egg say to the other?\nHeard any good yolks lately?"
+    ,
+    "195. What do you call a rabbit who tells jokes?\nA funny bunny."
+    ,
+    "196. What is a rabbit’s favorite dance?\nThe Bunny Hop."
+    ,
+    "197. What kind of jewelry do rabbits wear?\n14 carrot gold."
+    ,
+    "198. How many chocolate bunnies can you put into an empty Easter basket?\nOnly one because after that, it’s not empty."
+    ,
+    "199. What do you call a rabbit with fleas?\nBugs Bunny."
+    ,
+    "200. Why did the Easter egg hide?\nHe was a little chicken."
+    ,
+    "201. Why did the robber jump in the shower?\nHe wanted to make a clean getaway."
+    ,
+    "202. What kind of shoes do robbers wear?\nSneakers."]
+
+function getjoke() {
+    let jokeNumber = Number.parseInt(document.getElementById("jokeNum").value);
+    if (isNaN(jokeNumber)) {
+        not_a_number();
+        return;
+    } else {
+        console.log(jokeNumber - 1);
+        console.log(all_jokes[jokeNumber - 1]);
+        show_joke(all_jokes[jokeNumber - 1])
+    }
+}
+function show_joke(joke) {
+    // jokeBox
+    document.getElementById("jokeBox").value = joke;
+}
+function not_a_number() {
+
+    document.getElementById("jokeNum").style.background = "red";
+    document.getElementById("jokeNum").style.color = "white";
+    document.getElementById("jokeBox").value = "";
+    alert("Enter a valid number between 1 to 209")
+    document.getElementById("jokeNum").value = "";
+    document.getElementById("jokeNum").style.background = "white";
+    document.getElementById("jokeNum").style.color = "black";
+
+}
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
