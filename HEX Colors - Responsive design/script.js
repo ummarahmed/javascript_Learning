5ab8e7cc6bb948506e0dc981348106943d3d3fca
Ahmed Ummar <ummarahmed001@gmail.com>
2023-05-20 16:27:42 +0300

    HEX color codes
    Projects tells you the hax-code of background color. There is also an use of animation in this project.

---
+++ HEX Colors - Responsive design/script.js
@@ -0,0 +1,14 @@
+function changeColor(){
+    var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];// all possible values for hex color codes
+
+    var hexcode = '';
+    for(var i=0; i<6; i++){
+        var random_index = Math.floor(Math.random()*hex_numbers.length);
+        hexcode += hex_numbers[random_index]
+    }// get the random hex-code
+    
+    // set the color code to text and as background color
+    document.getElementById("hex-code").innerText = hexcode;
+    document.getElementsByTagName("body")[0].style.background = "#"+hexcode;
+
+}
