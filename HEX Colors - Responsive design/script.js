60d30768583591b3d17bc93a16fc2755343ac70f
Ahmed Ummar <ummarahmed001@gmail.com>
2023-05-23 18:49:13 +0300

    Update script.js

--- HEX Colors - Responsive design/script.js
+++ HEX Colors - Responsive design/script.js
@@ -2,6 +2,7 @@ function changeColor(){
     var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];// all possible values for hex color codes
 
     var hexcode = '';
+
     for(var i=0; i<6; i++){
         var random_index = Math.floor(Math.random()*hex_numbers.length);
         hexcode += hex_numbers[random_index]
@@ -10,7 +11,6 @@ function changeColor(){
     // set the color code to text and as background color
     document.getElementById("hex-code").innerText = hexcode;
     document.getElementsByTagName("body")[0].style.background = "#"+hexcode;
-    console.log("Hexcode: "+hexcode);
 
     var btn_hex = '';
     for(var i=0; i<6; i++){
